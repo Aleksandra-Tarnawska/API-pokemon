ce0c99e1469b8c63c1ad0f7f852efa39a455cec9
Aleksandra <atarnawska_95@wp.pl>
2023-11-15 14:19:37 +0100

    end

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 class PokemonTCGCatalog {
     constructor() {
-        this.cards = []
-        this.catalog = null;
+        this.pageSize = 4;
+        this.currentPage = 1;
+        this.cards = [];
+        this.newCards = 
 
+        this.catalog = null;
+        this.button = null;
+        this.loader = null;
+        this.search = null;
+        this.info = null;
 
         this.API = 'https://api.pokemontcg.io';
         this.API_VERSION = 'v2';
@@ -11,25 +18,50 @@ class PokemonTCGCatalog {
         this.API_ENDPOINT = `${this.API}/${this.API_VERSION}/${this.API_RESOURCE}`;
 
         this.UiSelectors = {
-            content: '[data-content]'
-        }
+            content: '[data-content]',
+            button: '[data-button]',
+            loader: '[data-loader]',
+            search: 'search',
+            card: '[data-card]',
+            info: '[data-info]',
+        };
     }
 
     initializeCatalog() {  
         this.catalog = document.querySelector(this.UiSelectors.content);
-        
+        this.button = document.querySelector(this.UiSelectors.button);
+        this.loader = document.querySelector(this.UiSelectors.loader);
+        this.search = document.getElementById(this.UiSelectors.search);
+        this.info = document.querySelector(this.UiSelectors.info);
+
+        this.addEventListeners();
         this.pullCards();
     }
 
-    async pullCards() {
-        const { data } = await this.fetchData(this.API_ENDPOINT);
- 
-        this.cards = [...data];
-
-        this.createCatalog(this.cards);
-        
-        console.log(data);
+    addEventListeners() {
+        this.button.addEventListener('click', () => this.pullCards());
+        this.search.addEventListener('keyup', () => this.filterCards());
     }
+
+    async pullCards() {
+        this.toggleShowElement(this.loader, this.button);
+        const { data } = await this.fetchData(
+            `${this.API_ENDPOINT}?page=${this.currentPage}&pageSize=${this.pageSize}`,
+        );
+        this.toggleShowElement(this.loader, this.button);
+
+        this.cards = [...this.cards, ...data];
+
+        this.newCards = [...data];
+
+        this.createCatalog(this.newCards);
+        this.currentPage++;
+    }
+
+    toggleShowElement(...elements) {
+        elements.forEach((element) => element.classList.toggle('hide'));
+    }
+
 
     async fetchData(url) {
         const response = await fetch(url);
@@ -39,24 +71,52 @@ class PokemonTCGCatalog {
     }
 
     createCatalog(data) {
-        this.catalog.innerHTML += [
+        this.catalog.insertAdjacentHTML('beforeend', [
             data.map((data) => this.createCard(data)).join(''),
-        ];
+        ]);
     }
-    createCard({name, number, images, supertype, subtypes, rarity}) {
+    createCard({name, number, images, supertype, subtypes, rarity, id }) {
         return `
-        <article class="card">
+        <article class="card" id=${id} data-card>
             <header class="card__header">
                 <h2 class="card__heading">
                     ${name}
                 </h2>
-                <p class="card__number">${number}</p>
+                <p class="card__number">Nr: ${number}</p>
             </header>
             <img class="card__image" src="${images.small}" alt="${name}"/>
-            <p class="card__description">Supertype: ${supertype}</p>
-            <p class="card__description">Subtype: ${subtypes}</p>
-            <p class="card__description">Rarity: ${rarity}</p>
+            <p class="card__description"><span class="bold">Supertype:</span> ${supertype}</p>
+            <p class="card__description ${
+                subtypes ? '' : 'hide'
+            }"><span class="bold">Subtype:</span> ${subtypes}</p>
+            <p class="card__description ${
+                rarity ? '' : 'hide'
+            }"><span class="bold">Rarity:</span>  ${rarity}</p>
         </article>
-        `
+        `;
     }
-}
+
+  filterCards() {
+    const searchQuery = this.search.value.toLowerCase();
+
+    searchQuery.length 
+    ? this.button.classList.add('hide') 
+    : this.button.classList.remove('hide');
+    
+    document
+    .querySelectorAll(this.UiSelectors.card)
+    .forEach((el) => el.classList.remove('hide'));
+
+    const filteredCards = this.cards.filter(
+        ({ name }) => !name.toLowerCase().includes(searchQuery),
+    );
+
+    filteredCards.length === this.cards.length
+      ? this.info.classList.remove('hide')
+      : this.info.classList.add('hide');
+      
+    filteredCards.forEach(({ id }) =>
+        document.getElementById(id).classList.add('hide'),
+    );
+  }  
+} 
